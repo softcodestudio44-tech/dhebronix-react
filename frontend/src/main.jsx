740a57b4dae2176b380d452a871b10f93b154129
Softcode <softcodestudio44@gmail.com>
2026-07-03 03:19:41 +0100

    update new files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import { HelmetProvider } from 'react-helmet-async'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <HelmetProvider>
         <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
+      </HelmetProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
